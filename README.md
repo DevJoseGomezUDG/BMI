b7ce856f7a4f5371d5b67bbd3c4b41423961a66e
devjosegomezUDG <jose.gcamacho@udgvirtual.udg.mx>
2022-03-17 13:20:22 -0600

    BMI calculator 0.0.1

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# Getting Started with Create React App
+# BMI Calculator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
+## Run project
+### cd bmi
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [_HEIGHT, setHeight] = useState(0);
+  const [_MASS, setMass] = useState(0);
+  const [_BMI, setBmi] = useState(0);
+  const [_RESULT, setResult] = useState('');
+  let inputType = "number"
+  const labels = { height: 'height (meters)', mass: 'mass (kg)'}
+  const valuesHeight = { min: 1, max: 2.50, step: 0.01 }
+
+  const categories = 
+  [
+    'Delgadez severa',
+    'Delgadez moderada',
+    'Delgadez aceptable',
+    'Peso normal',
+    'Sobrepeso',
+    'Obesidad leve (Tipo I)',
+    'Obesidad media (Tipo II)',
+    'Obesidad mórbida (Tipo III)'
+  ]
+
+
+  const calculate = (e) => {
+    e.preventDefault();
+    const formValid = +_HEIGHT > 0 && +_MASS > 0;
+    if (!formValid) {
+      return;
+    }
+    const _BMI = +_MASS / ( +_HEIGHT ) ** 2;
+    setBmi(parseFloat(_BMI).toFixed(2));
+    
+    if(_BMI < 16) setResult(categories[0])
+    if(_BMI >=16 && _BMI <17) setResult(categories[1])
+    if(_BMI >=17 && _BMI <=18.5) setResult(categories[2])
+    if(_BMI > 18.5 && _BMI <=25) setResult(categories[3])
+    if(_BMI > 25 && _BMI <=30) setResult(categories[4])
+    if(_BMI > 30 && _BMI <=35) setResult(categories[5])
+    if(_BMI > 35 && _BMI <=40) setResult(categories[6])
+    if(_BMI > 40) setResult(categories[7])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="box">
+      <form onSubmit={calculate}>
+        <>
+          <label>{ labels.height.toUpperCase() }</label> <br />
+          <input type={ inputType } min={ valuesHeight.min } max={ valuesHeight.max } step={ valuesHeight.step } value={ _HEIGHT } onChange={(e) => setHeight( e.target.value )} />
+        </> <br />
+        <>
+          <label>{ labels.mass.toUpperCase() }</label> <br />
+          <input type={ inputType } value={ _MASS } onChange={(e) => setMass( e.target.value )} />
+        </> <br />
+        <button type="submit">CALCULATE BMI</button>
+      </form>
+      <p>{`Your BMI is ${ _BMI }: ${ _RESULT }`}</p>
     </div>
   );
 }
-
-export default App;
